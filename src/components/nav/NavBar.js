7fa3c54973e0344c92b08cf442c8d8f04d5ebefd
Chesney Hardin <chesney.hardin@gmail.com>
2023-06-02 15:51:49 -0500

    finding candy input form

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,24 +1,23 @@
-import { Link, useNavigate } from "react-router-dom"
+import { CustomerNav } from "./CustomerNav"
+import { EmployeeNav } from "./EmployeeNav"
 import "./NavBar.css"
 
 export const NavBar = () => {
-    const navigate = useNavigate()
 
-    return (
-        <ul className="navbar">
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/locations">Locations</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/products">Products</Link>
-            </li>
-            <li className="navbar__item navbar__logout">
-                <Link className="navbar__link" to="" onClick={() => {
-                    localStorage.removeItem("kandy_user")
-                    navigate("/", {replace: true})
-                }}>Logout</Link>
-            </li>
-        </ul>
-    )
+    const localKandyUser = localStorage.getItem("kandy_user")
+    const kandyUserObject = JSON.parse(localKandyUser)
+
+	if (kandyUserObject.staff) {
+		// Return Employee views
+		return <EmployeeNav />
+	}
+
+	else {
+		// Return customer views
+		return <CustomerNav />
+	}
 }
 
+/* Change this to check if kandy_user is a customer 
+or employee and direct to the corresponding navbar 
+*/
